b46cf70e701ea71fdd3e78b368ad45a4c9b4af32
WavixSan <wavix@mail.ru>
2016-11-29 09:19:10 +0200

    4. Дан массив $arr. С помощью первого цикла foreach выведите на экран столбец ключей, с помощью второго — столбец элементов.
    $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой')?

---
+++ arrays_loops_tasks/4.php
@@ -0,0 +1,9 @@
+<?php
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+foreach($arr as $k=>$v){
+    echo "$k<br>";
+}
+echo "<br>";
+foreach($arr as $v){
+    echo "$v<br>";
+}
