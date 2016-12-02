febadc703e47cf1ae6c7d952bf0a07e083def7a9
WavixSan <wavix@mail.ru>
2016-12-02 09:34:42 +0200

    Домашнее задание от 30.11.2016

---
+++ arrays_loops_tasks/26.php
@@ -0,0 +1,5 @@
+<?php
+$arr=array();
+for($i=1;$i<=10;$i++){
+    $arr[]=rand(1,100);
+}
---
+++ arrays_loops_tasks/27.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 30.11.2016
+ * Time: 18:30
+ */
