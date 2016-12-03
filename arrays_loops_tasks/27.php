24520fc2b97c7f5a2e5145892cdc6d8d87cc822f
WavixSan <wavix@mail.ru>
2016-12-03 02:16:37 +0200

    27. Создать генератор случайных таблиц. Есть переменные: $row - кол-во строк в таблице, $cols - кол-во столбцов в таблице. Есть список цветов, в массиве: $colors = array('red','yellow','blue','gray','maroon','brown','green'). Необходимо создать скрипт, который по заданным условиям выведет таблицу размера $rows на $cols, в которой все ячейки будут иметь цвета, выбранные случайным образом из массива $colors. В каждой ячейке должно находиться случайное число.

--- arrays_loops_tasks/27.php
+++ arrays_loops_tasks/27.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 30.11.2016
- * Time: 18:30
- */
+$rows=5;
+$cols=5;
+$colors = array('red','yellow','blue','gray','maroon','brown','green');
+
+echo "<table>";
+for($i=1;$i<=$cols;$i++){
+    echo "<tr>";
+    for($j=1;$j<=$rows;$j++){
+        $color=rand(0,count($colors)-1);
+        $number=rand(1000,9999);
+        echo "<td style='background: {$colors[$color]};'>$number</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
